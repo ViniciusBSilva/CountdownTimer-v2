7e4dd853a90754aed63377cfc0ec8cbfb72f2c00
Vinicius Bernardes Silva <vinicius.bsilva@hotmail.com>
2021-11-05 17:45:02 -0300

    Buttons added to countdown section

--- CountdownTimerv2.js
+++ CountdownTimerv2.js
@@ -9,6 +9,7 @@ for (let i = 0; i < radioSelTypes.length; i++) {
 
         console.log(showConfigBlock(this.value));
         document.getElementById("configTimer").innerHTML = showConfigBlock(this.value);
+        document.getElementById("timer").innerHTML = "";
 
     };
 
@@ -19,35 +20,23 @@ function showConfigBlock(selType) {
     let content = "";
 
     content = "<p><strong>Timer Configuration</strong></p>" +
-              "<p class='configForm'>";
+        "<p class='configForm'>";
 
     switch (selType) {
 
         case "dateTime":
             content +=
                 "<form name='dateTimeForm'>" +
-                "<p>" +
-                "<label>Date/Time " +
-                "<input type='datetime-local' step='1'>" +
-                "</label>" +
-                "</p>" +
-                "<p>" +
-                "<button class='botao' type='submit' onsubmit='showDateTimeTimer()'>Start!</button>" +
-                "</p>" +
+                "<p><label>Date/Time <input type='datetime-local' step='1'></label></p>" +
+                "<p><button class='button' type='button' onclick='showDateTimeTimer()'>Start</button></p>" +
                 "</form>";
             break;
 
         case "amount":
             content +=
                 "<form name='amountForm'>" +
-                "<p>" +
-                "<label>Time " +
-                "<input type='time' step='1'>" +
-                "</label>" +
-                "</p>" +
-                "<p>" +
-                "<button class='botao' type='submit' onsubmit='showAmountTimer()'>Start!</button>" +
-                "</p>" +
+                "<p><label>Time <input type='time' step='1'></label></p>" +
+                "<p><button class='button' type='button' onclick='showAmountTimer()'>Start</button></p>" +
                 "</form>";
             break;
 
@@ -74,15 +63,20 @@ function showTimerBlock(timerType) {
     let content = "";
 
     content = "<p><strong>Timer</strong></p>" +
-              "<p class='timer'>";
+        "<p class='timer'>";
 
     switch (timerType) {
         case "dateTime":
             content += "dateTime";
+            content += "<p><button class='button' type='button' onclick='stopCountdown()'>Stop</button></p>";
+
             break;
 
         case "amount":
             content += "amount";
+            content += "<p><button class='button' type='button' onclick='startCountdown()'>Start</button> " + 
+                       "<button class='button' type='button' onclick='stopCountdown()'>Stop</button> " +
+                       "<button class='button' type='button' onclick='resetCountdown()'>Reset</button></p>";
             break;
 
         default:
