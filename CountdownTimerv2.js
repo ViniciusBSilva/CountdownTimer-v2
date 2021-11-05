b8d566b1d53af110bb09ae902282a1653db2c247
Vinicius Bernardes Silva <vinicius.bsilva@hotmail.com>
2021-11-05 16:08:18 -0300

    showTimerBlock() and styles initiated

--- CountdownTimerv2.js
+++ CountdownTimerv2.js
@@ -5,38 +5,91 @@ let prev = null;
 
 for (let i = 0; i < radioSelTypes.length; i++) {
 
-    console.log(radioSelTypes[i]);
     radioSelTypes[i].onclick = function () {
 
-        // (prev) ? console.log("prev: " + prev.value) : null;
-        // if (this !== prev) {
-        //     prev = this;
-        // }
-        // console.log("this: " + this.value);
-
-        document.getElementById("configTimer").innerHTML = showBlock(this.value);
+        console.log(showConfigBlock(this.value));
+        document.getElementById("configTimer").innerHTML = showConfigBlock(this.value);
 
     };
 
 }
 
-function showBlock(selType) {
+function showConfigBlock(selType) {
 
     let content = "";
+
+    content = "<p><strong>Timer Configuration</strong></p>" +
+              "<p class='configForm'>";
 
     switch (selType) {
 
         case "dateTime":
-            content = "<form name='dateTimeForm'><p>Date/Time <label><input type='datetime-local' step='1'></label></p></form>";
+            content +=
+                "<form name='dateTimeForm'>" +
+                "<p>" +
+                "<label>Date/Time " +
+                "<input type='datetime-local' step='1'>" +
+                "</label>" +
+                "</p>" +
+                "<p>" +
+                "<button class='botao' type='submit' onsubmit='showDateTimeTimer()'>Start!</button>" +
+                "</p>" +
+                "</form>";
             break;
 
         case "amount":
-            content = "<form name='amountForm'><p>Time <label><input type='time' step='1'></label></p></form>";
+            content +=
+                "<form name='amountForm'>" +
+                "<p>" +
+                "<label>Time " +
+                "<input type='time' step='1'>" +
+                "</label>" +
+                "</p>" +
+                "<p>" +
+                "<button class='botao' type='submit' onsubmit='showAmountTimer()'>Start!</button>" +
+                "</p>" +
+                "</form>";
             break;
 
         default:
             break;
     }
+
+    content += "</p>";
+
+    return content;
+
+}
+
+function showDateTimeTimer() {
+    document.getElementById("timer").innerHTML = showTimerBlock("dateTime");
+}
+
+function showAmountTimer() {
+    document.getElementById("timer").innerHTML = showTimerBlock("amount");
+}
+
+function showTimerBlock(timerType) {
+
+    let content = "";
+
+    content = "<p><strong>Timer</strong></p>" +
+              "<p class='timer'>";
+
+    switch (timerType) {
+        case "dateTime":
+            content += "dateTime";
+            break;
+
+        case "amount":
+            content += "amount";
+            break;
+
+        default:
+            break;
+    }
+
+    content += "</p>";
 
     return content;
 
